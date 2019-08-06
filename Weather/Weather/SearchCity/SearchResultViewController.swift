9d475fe91d464c02a3197e9795e6237bf858f711
NohEunTae <dmsxoeldk@naver.com>
2019-08-06 23:56:02 +0900

    도시 검색 기능 #1
    request 요청에 대한 부하 줄이기
    - 검색한 것을 dictionary에 저장
    - 검색한 것과 동일한 텍스트의 경우 저장했던 데이터 제공
    - 검색한 것을 포함하고 있는 텍스트의 경우 텍스트 내용을 포함하는 내용을 필터링하고, 이를 제외한 것만 request 요청

--- Weather/Weather/SearchCity/SearchResultViewController.swift
+++ Weather/Weather/SearchCity/SearchResultViewController.swift
@@ -24,6 +24,8 @@ class SearchResultViewController: UIViewController {
     @IBOutlet weak var resultTableView: UITableView!
     private let completer = MKLocalSearchCompleter()
     var matchingItems: [MKMapItem] = []
+    
+    var memorizingItems: [String : [MKMapItem]] = [:]
     weak var timer: Timer? = nil
     
     weak var delegate: SearchResultViewControllerDelegate? = nil
@@ -76,20 +78,35 @@ extension SearchResultViewController: UITableViewDataSource {
 
 extension SearchResultViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        
         guard let searchBarText = searchController.searchBar.text else { return }
-        if timer != nil {
-            timer?.invalidate()
-            timer = nil
-        }
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: searchBarText, repeats: false)
+        
         if completer.isSearching {
             self.completer.cancel()
         }
-
-        DispatchQueue.main.async {
-            self.resultTableView.isHidden = true
-            self.searchIndicator.startAnimating()
+        
+        if let isMemorized = memorizingItems[searchBarText] {
+            matchingItems = isMemorized
+            DispatchQueue.main.async {
+                self.resultTableView.isHidden = false
+                self.searchIndicator.stopAnimating()
+                self.resultTableView.reloadData()
+            }
+        } else {
+            let maybeInclude = memorizingItems.keys.filter { searchBarText.contains($0)}.last
+            if let include = maybeInclude {
+                let maybeItems = self.memorizingItems[include]?.filter { $0.placemark.title != nil}.filter { $0.placemark.title!.contains(searchBarText)}
+                if let items = maybeItems {
+                    self.memorizingItems.updateValue(items, forKey: searchBarText)
+                    matchingItems = items
+                }
+            }
+            timer?.invalidate()
+            timer = nil
+            timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(timerFunc), userInfo: searchBarText, repeats: false)
+            DispatchQueue.main.async {
+                self.resultTableView.isHidden = true
+                self.searchIndicator.startAnimating()
+            }
         }
     }
     
@@ -115,20 +132,25 @@ extension SearchResultViewController: MKLocalSearchCompleterDelegate {
                 }
             }
         }
-        
-        print(maybeCities.count)
 
+        if let memorizedItems = memorizingItems[completer.queryFragment] {
+            self.matchingItems.append(contentsOf: memorizedItems)
+            for item in memorizedItems {
+                let maybeInclude = maybeCities.filter { $0.contains(item.placemark.title!)}
+                for item in maybeInclude {
+                    maybeCities.remove(at: maybeCities.firstIndex(of: item)!)
+                }
+            }
+        }
+        
         let group = DispatchGroup()
         let request = MKLocalSearch.Request()
         for maybeCity in maybeCities {
             group.enter()
-            
             request.naturalLanguageQuery = maybeCity
-
             let search = MKLocalSearch(request: request)
             search.start { [unowned self] response, error in
                 guard let response = response else {
-                    print(error?.localizedDescription)
                     group.leave()
                     return
                 }
@@ -143,7 +165,7 @@ extension SearchResultViewController: MKLocalSearchCompleterDelegate {
         }
         
         group.notify(queue: .main) {
-            print(self.matchingItems.count)
+            self.memorizingItems.updateValue(self.matchingItems, forKey: completer.queryFragment)
             self.searchIndicator.stopAnimating()
             self.resultTableView.isHidden = false
             self.resultTableView.reloadData()
